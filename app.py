2ca995525523d0a9409420b37e21cacd37744798
SivaSinaga <sivasinaga1105@gmail.com>
2026-05-17 00:59:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -44,10 +44,5 @@ def verify():
         return jsonify({"valid": False})
 
 if __name__ == "__main__":
-    # Buat SSL context
-import os   ← tambahkan di baris paling atas
-
-# bagian bawah app.py:
-if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
